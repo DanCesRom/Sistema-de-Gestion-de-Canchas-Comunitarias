10d10e4d88e7a894e95ff7109ea83d5d87682583
DanCesRom <123031318+DanCesRom@users.noreply.github.com>
2025-08-13 15:19:58 -0400

    .

--- staticfiles/js/help/faq.js
+++
@@ -1,7 +0,0 @@
-// Toggle FAQ answers
-document.querySelectorAll('.faq-question').forEach(q => {
-    q.addEventListener('click', () => {
-        const answer = q.nextElementSibling;
-        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
-    });
-});
